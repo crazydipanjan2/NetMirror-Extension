9033553212d331320bb94e8c5239393c5c911af7
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2026-05-20 17:46:12 +0545

    Resolved #15: Remove the use of gson.

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,7 @@ subprojects {
         implementation("com.squareup.okhttp3:okhttp:4.12.0")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
         implementation("org.mozilla:rhino:1.8.0")
-        implementation("com.google.code.gson:gson:2.11.0")
+        // implementation("com.google.code.gson:gson:2.11.0")
         implementation("androidx.annotation:annotation:1.7.1")
     }
 }
